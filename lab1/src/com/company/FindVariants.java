5bef20ec924525b7f9b537e211b11254dcd0cf3f
andrey_bolduyrev <andrey.kireev23@mail.ru>
2018-11-29 10:02:32 +0300

    first commit

---
+++ lab1/src/com/company/FindVariants.java
@@ -0,0 +1,59 @@
+package com.company;
+//класс, проверяющий, какие комбинации подходят
+
+class FindVariants {
+    private int ans;
+    private int[] arr;
+    FindVariants(int ans, int[] arr){
+        this.ans=ans;
+        this.arr=arr;
+    }
+    String findVariants() {
+        //ищем все варианты
+        PermutationsWithRepetition gen = new PermutationsWithRepetition(
+                new char[]{'-', '+', '/', '*',' '},
+                arr.length - 1);
+        char[][] variations = gen.getVariations();  //получили все возможные ккомбинации из "-", "+", "/", "*"
+
+        //для подсчёта значения выражения
+        Calc c=new Calc();
+
+        //ищем подходящие комбинации
+
+        String resStr = "";
+        String calcStr;
+        for (char [] variation : variations) {  //проходим по всем комбинациям
+            int j = 0; //индекс позиции в комбинации.
+
+            //собираем выражение в одну строку
+            calcStr=String.valueOf(arr[0]); // первый символ строки это первая цифра
+            for (int i = 1; i < arr.length; i++) {  //цикл по числам, для которых нужно подобрать знаки
+                switch (variation[j]) {   //добавляем знаки
+                    case '+':
+                        calcStr = calcStr+ '+'+ String.valueOf(arr[i]);
+                        break;
+                    case '-':
+                        calcStr = calcStr +'-'+ String.valueOf(arr[i]);
+                        break;
+                    case '*':
+                        calcStr = calcStr +'*'+ String.valueOf(arr[i]);
+                        break;
+                    case '/':
+                        calcStr = calcStr +'/'+ String.valueOf(arr[i]);
+                        break;
+                    case ' ':
+                        calcStr+= String.valueOf(arr[i]);
+                }
+                j++;
+            }
+            String res=c.calc(calcStr);  //получившуюся строку считаем
+
+            //если полученнный ответ и требуемый равны, то запоминаем комбинацию в resStr
+            if (Character.isDigit(res.toCharArray()[0]))
+                if (Integer.parseInt(res) == ans) {
+                    resStr += calcStr + " = " + ans + "\n";
+                }
+        }
+        return resStr;
+    }
+}
